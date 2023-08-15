faae85446f220c788892cc555674ef0c801d32b1
Pablo Couto <pablocouto@aluno.senai.br>
2023-08-15 14:12:10 -0300

    feat: progress in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,59 +6,35 @@ class Task {
     }
 
 }
-
 class TaskList {
     constructor() {
         this.tasks = []
     }
-
-    savePost() {
-        const title = document.getElementById("title").value;
-        const alert = document.getElementById("p-alert").value;
-
-        if(title == " ") {
-            alert = document.getElementById("p-alert").classList.remove("hidden");
-        } else {
-
-        }
-    }
-
-    mathMethods() {
-        return Math.floor(Math.random() * 9999)
-    }
-
-    cleanFields() {
-        title = document.getElementById("title").value = "";
-    }
-
-    storePosts(id, title, status) {
-        const post = new Task(
-            id,
-            title,
-            status
-        )
-        this.tasks.push(post)
-    }
-
     
+    addPost(task) {
+        this.tasks.push(task)
+    }
+
+    deletePost(index) {
+        this.tasks.splice(index, 1);
+    }
+    
+
 }
 
-function showPosts() {
-    document.getElementById("list").classList.remove("hidden");
+function randonId() {
+    return Math.floor(Math.random() * 9999)
+}
 
-    let showContent = "";
+const taskList = new TaskList()
 
-    this.tasks.forEach((post, index) => {
-        showContent += `
-    <div id="task">
-        <h2>${post.title}</h2>
-        <div class="buttons">
-            <button class="task-btn" id="verified"><i class="fa-solid fa-check"></i></button>
-            <button class="task-btn" id="pencil" onclick="editPost(${index})"><i class="fa-solid fa-pencil"></i></button>
-            <button class="task-btn" id="trash" onclick="deletePost(${index})"><i class="fa-solid fa-trash"></i></button>
-        </div>
-    </div>`
-    })
+function showPost() {
+    let title = document.getElementById("title").value;
 
-    document.getElementById("list").innerHTML = showContent
+    const novo = new Task(randonId(), title, false)
+    taskList.addPost(novo)
+
+    title = document.getElementById("title").value = "";
+    console.log(taskList.tasks)
+    console.log(novo)
 }

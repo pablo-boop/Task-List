4c6d044a2d1202df57ecb0db8bfc55b9fbd50091
Pablo Couto <pablocouto@aluno.senai.br>
2023-08-15 14:24:30 -0300

    feat: saving and showing posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,12 @@ class TaskList {
     deletePost(index) {
         this.tasks.splice(index, 1);
     }
+
+    editPost(index) {
+        const post = taskList.tasks[index]
+
+        document.getElementById("title").value = post.title;
+    }
     
 
 }
@@ -30,11 +36,24 @@ const taskList = new TaskList()
 
 function showPost() {
     let title = document.getElementById("title").value;
-
+    document.getElementById("list").classList.remove("hidden");
     const novo = new Task(randonId(), title, false)
     taskList.addPost(novo)
 
+    let showContent = "";
+
+    taskList.tasks.forEach((novo) => {
+        showContent += `
+        <div id="task">
+        <h2>${novo.title}</h2>
+        <div class="buttons">
+            <button class="task-btn" id="verified"><i class="fa-solid fa-check"></i></button>
+            <button class="task-btn" id="pencil" onclick="editPost()"><i class="fa-solid fa-pencil"></i></button>
+            <button class="task-btn" id="trash" onclick="deletePost()"><i class="fa-solid fa-trash"></i></button>
+        </div>
+    </div>`
+    })
+
     title = document.getElementById("title").value = "";
-    console.log(taskList.tasks)
-    console.log(novo)
+    document.getElementById("list").innerHTML = showContent;
 }
